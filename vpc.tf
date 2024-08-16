75db185d6696e346d41f8c15741ea97753ae3de9
Sandip Gangapadhyay <gangapad@amazon.com>
2024-08-16 15:08:08 -0400

    Enable VPC flowlog

--- vpc.tf
+++ vpc.tf
@@ -15,4 +15,13 @@ module "vpc" {
     "kubernetes.io/role/elb" = 1
   }
   enable_nat_gateway = true
+  # Cloudwatch log group and IAM role will be created
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+
+  flow_log_max_aggregation_interval         = 60
+  # flow_log_cloudwatch_log_group_name_prefix = "/aws/var.vpc_name/"
+  flow_log_cloudwatch_log_group_name_suffix = var.vpc_name
+  flow_log_cloudwatch_log_group_class       = "INFREQUENT_ACCESS"
 }
